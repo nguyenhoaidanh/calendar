1be82b36d74851b5163582b48f423a8c81d54ff8
hoaidanh <nguyenhoaidanh2096@gmail.com>
2018-06-28 21:25:45 +0700

    s

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/:month/:year',function(req,res){
 		var data={month:req.params.month,year:req.params.year};
 		res.render('index',{data:data});
 });
-
-app.listen(process.env.port||8080);
+app.set('port', process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 console.log("Create server done");
